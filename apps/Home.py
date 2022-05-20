0ee52af4e45fc89b5425486ab1de277bbb53efb6
nourelsaadi <73889391+nourelsaadi@users.noreply.github.com>
2022-05-20 10:40:36 +0300

    Update Home.py

--- apps/Home.py
+++ apps/Home.py
@@ -15,7 +15,7 @@ def app():
      #if the data is uploaded, read the uploaded file & store into another csv file within the folder
      if uploaded_file is not None:
           data = pd.read_csv(uploaded_file)
-          data.to_csv(r'C:\Users\itm\Desktop\Multipage\apps\main_data.csv', index=False) #considering this is a multiapp, this was my go to method to carry the dataset to the other pages (by thus saving the file into a csv that is then saved in the folder)
+          data.to_csv(r'data/main_data.csv', index=False) #considering this is a multiapp, this was my go to method to carry the dataset to the other pages (by thus saving the file into a csv that is then saved in the folder)
      else:
           data = ""
 
